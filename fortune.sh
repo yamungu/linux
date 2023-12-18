75798339cd3383f78985bd60d00caab75adc93f7
yamungu <105924523+yamungu@users.noreply.github.com>
2023-12-18 11:04:33 +0300

    Add files via upload

---
+++ fortune.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#Program to tell a persons fortune
+
+
+echo -e "\n~~ Fortune Teller ~~\n"
+
+RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
+
+echo ${RESPONSES[5]}
+
+N=$(( RANDOM % 6 ))
+
+echo ${RESPONSES[$N]}
+
+FUNCTION_NAME() {
+  STATEMENTS
+}
+
+
+
+function GET_FORTUNE() {
+ if [[ ! $1 ]]
+then
+  echo Ask a yes or no question:
+else
+  echo Try again. Make sure it ends with a question mark:
+fi
+
+  read QUESTION
+}
+
+
+until [[ $QUESTION =~ \?$ ]]
+do
+  GET_FORTUNE again
+done
+
+echo -e "\n${RESPONSES[$N]}"
+
+
